ad3cf483621ecd635b5ed2f80086bc3fde33c617
ray grant <rgrant8@mail.csuchico.edu>
2015-03-19 23:32:18 -0700

    git test

--- app/src/main/java/com/example/yass8n/whozthis/activities/MainActivity.java
+++ app/src/main/java/com/example/yass8n/whozthis/activities/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //https://radiant-inferno-906.firebaseio.com   this is the URL where our data will be stored
         super.onCreate(savedInstanceState);
-        context = monkey_man;
+        context = HEY_DICK_HEAD!;
         Firebase.setAndroidContext(this);
         firebase = new Firebase("https://radiant-inferno-906.firebaseio.com/");
         setContentView(R.layout.activity_main);
